3e3db6b7b25395a4276c8d6cb81c283374dccc13
adias94 <diasr.al94@yahoo.com>
2018-01-06 18:35:57 -0500

    Grayscale Fix

--- Sidebar.js
+++ Sidebar.js
@@ -213,6 +213,7 @@ CreatureStatsBlock.prototype = {
     grayscaleImage: function() {
         this._image.style.filter = "grayscale(100%)";
         this._image.style.filter = "gray";
+        this._image.style.filter =  "url('data:image/svg+xml;utf8,&lt;svg xmlns=\'http://www.w3.org/2000/svg\'&gt;&lt;filter id=\'grayscale\'&gt;&lt;feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/&gt;&lt;/filter&gt;&lt;/svg&gt;#grayscale')";
     },
 };
 
