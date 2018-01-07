ba1291ee259901aee848f35a83a85459682ed29c
adias94 <diasr.al94@yahoo.com>
2018-01-07 10:07:37 -0500

    ...

--- Misc.js
+++ Misc.js
@@ -176,6 +176,7 @@ var AnimatedSprite = function(spriteFileName_, sheetFileName_, tileLength_, tile
     this._sheet = this._setSheet();  //sprite sheet used to animate creature during movement
     this._sheet.onload = function() {
         this._numFrames = (this._sheet.width / this._image.width);
+        console.log(this._numFrames);
     }.bind(this);
 };
 AnimatedSprite.prototype = Object.create(Sprite.prototype);
