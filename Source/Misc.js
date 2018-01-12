6d3b399a8cfdbcdb7d69ec95b4e21bbf4f5ac3d6
adias94 <diasr.al94@yahoo.com>
2018-01-12 10:39:16 -0500

    ...

--- Source/Misc.js
+++ Source/Misc.js
@@ -172,13 +172,9 @@ ObstacleSprite.prototype._directory = "images/obstacles/";
 
 var AnimatedSprite = function(spriteFileName_, sheetFileName_, tileLength_, tileWidth_) {
     Sprite.call(this, spriteFileName_, tileLength_, tileWidth_);
+    this._numFrames = 3;
     this._sheetFileName = sheetFileName_;
     this._sheet = this._setSheet();  //sprite sheet used to animate creature during movement
-    this._image.onload = function() {
-        this._sheet.onload = function() {
-            this._numFrames = (this._sheet.width / this._image.width);
-        }.bind(this);
-    }.bind(this);
 };
 AnimatedSprite.prototype = Object.create(Sprite.prototype);
 AnimatedSprite.prototype.constructor = AnimatedSprite;
